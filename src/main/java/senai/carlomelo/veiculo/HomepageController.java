b0d96019732e19256797a53bf373b378fb1b8ebd
Kaique Bahmad <kaiquebahmadt@gmail.com>
2025-10-21 21:16:44 -0300

    testes selenium

--- src/main/java/senai/carlomelo/veiculo/HomepageController.java
+++ src/main/java/senai/carlomelo/veiculo/HomepageController.java
@@ -20,33 +20,7 @@ public class HomepageController {
     @GetMapping("")
     public String hello(Model model) {
         List<Veiculo> veiculos = repository.findAll();
-        if (veiculos == null || veiculos.size() == 0) {
-            Veiculo v1 = new Veiculo();
-            v1.setDescricao("Carro Esportivo");
-            v1.setFabricante("Ferrari");
-            v1.setCor("Vermelho");
-            v1.setPlaca("FERR-1234");
-            repository.save(v1);
-
-            Veiculo v2 = new Veiculo();
-            v2.setDescricao("Sedan Familiar");
-            v2.setFabricante("Toyota");
-            v2.setCor("Preto");
-            v2.setPlaca("TOYO-5678");
-            repository.save(v2);
-
-            Veiculo v3 = new Veiculo();
-            v3.setDescricao("SUV Compacto");
-            v3.setFabricante("Honda");
-            v3.setCor("Branco");
-            v3.setPlaca("HOND-9012");
-            repository.save(v3);
-
-            veiculos = repository.findAll();
-        }
-
         model.addAttribute("veiculos", veiculos);
-
         return "hello";
     }
 
